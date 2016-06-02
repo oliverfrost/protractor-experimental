0c3f6dbf2eaf5883620af0b0dc523d6094ced842
Sergey Kholodniy <sekh@ciklum.com>
2016-06-02 13:45:03 +0300

    Fixes create wrestler test by changing DOB parameter. Also adds new asserts and click action.

--- test/specs/create-wrestler.spec.js
+++ test/specs/create-wrestler.spec.js
@@ -10,20 +10,24 @@ describe('Create new wrestler', function () {
         browser.manage().deleteAllCookies();
         LoginPage.login(user.login, user.password);
     });
-    
+
 
     it('Create button should be active', function () {
         WrestlersListPage.newWrestler();
         WrestlerPage.typeLastName(iWrestler.lastName);
         WrestlerPage.typeFirstName(iWrestler.firstName);
-        WrestlerPage.typeDateOfBirth("07-10-90");
+        WrestlerPage.typeDateOfBirth("07-10-1990");
         WrestlerPage.typeMiddleName(iWrestler.middle);
         WrestlerPage.selectRegion("AR Krym");
         WrestlerPage.selectFst("Kolos");
         WrestlerPage.selectStyle("FS");
         WrestlerPage.selectAge("Cadet");
         WrestlerPage.selectYear(iWrestler.year);
+        WrestlerPage.clickCreateButton();
 
-        expect(WrestlerPage.isCreateButtonClickable()).toBeTruthy("Create button should be active");
+
+        expect(WrestlerPage.isCreateButtonClickable()).toBeFalsy();
+        expect(WrestlerPage.photoDivHeader.isDisplayed()).toBeTruthy();
+        expect(WrestlerPage.documentsDivHeader.isDisplayed()).toBeTruthy();
     });
 });
